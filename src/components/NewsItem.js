147df6d57630ef156d0b015b9e7d0f718ec8b461
rizwan737 <89721796+rizwan737@users.noreply.github.com>
2023-08-10 11:36:22 +0500

    Setup Components and Added Bootstrap Navbar

---
+++ src/components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>NewsItem Component</div>
+    )
+  }
+}
+
+export default NewsItem
