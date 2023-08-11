603e4f9c36f63914bef561f161b1d04cc6a4393e
rizwan737 <89721796+rizwan737@users.noreply.github.com>
2023-08-11 10:42:06 +0500

    Added Categorywise News and set proptypes

--- src/components/News.js
+++ src/components/News.js
@@ -4,7 +4,7 @@ import Spinner from './Spinner';
 import PropTypes from 'prop-types';
 export class News extends Component {
     static defaultProps = {
-        country: ''
+        country: 'in',
     }
 
     static propTypes = {
