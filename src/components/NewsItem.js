f48b27e288515db4ce8d8436bbf7fe1cb5bd5165
rizwan737 <89721796+rizwan737@users.noreply.github.com>
2023-08-11 10:41:09 +0500

    Added Categorywise News and set proptypes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-  render() {
-    let {title,description,imageUrl,newsUrl} = this.props;
-    return (
-        <div className="card" style={{width: "18rem"}}>
-        <img src={imageUrl} className="card-img-top" alt="..."/>
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{description}</p>
-          <a href={newsUrl} target='_blank' rel='noreferrer' className="btn btn-sm btn-dark">Read More</a>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+        return (
+            <div className="card mt-4" style={{ width: "18rem" }}>
+                <img src={imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}  <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
+                        {source}
+                    </span></h5>
+                    <p className="card-text">{description}</p>
+                    <p className="card-text"><small className='text-muted'>By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+                    <a href={newsUrl} target='_blank' rel='noreferrer' className="btn btn-sm btn-dark">Read More</a>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default NewsItem
